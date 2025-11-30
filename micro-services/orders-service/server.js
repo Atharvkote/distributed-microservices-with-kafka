2bc30d5f7842c49e579812b8fd6cf0719a90e904
Atharvkote <atharvkote3@gmail.com>
2025-11-30 19:54:38 +0530

    new commiy

--- micro-services/orders-service/server.js
+++ micro-services/orders-service/server.js
@@ -18,5 +18,5 @@ app.use((req, res, next) => {
 
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on http://localhost:${PORT}`)
+  console.log(`Server is running on http://localhost:${PORT}`)
 })
